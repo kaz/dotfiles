0e4b059d8b31b61ee5676698e8e565a41a0ca7b6
Kazuki Sawada <kazuki@6715.jp>
2019-09-10 20:40:17 +0900

    update gcloud scripts

--- files/profile.d/gcloud.sh
+++ files/profile.d/gcloud.sh
@@ -1,12 +1,16 @@
+gceup () {
+	gcloud compute instances create $1 --image-project=arch-linux-gce --image-family=arch --machine-type=n1-standard-1 --boot-disk-size=25GB --preemptible
+}
+
 gssh () {
 	fetch_list () {
 		gcloud compute instances list --format json
 	}
 	get_stat () {
-		jq -r "[.[]  | {key: .name, value: .status}] | from_entries.\"$1\""
+		jq -r "[.[] | {key: .name, value: .status}] | from_entries.\"$1\""
 	}
 	get_addr () {
-		jq -r "[.[]  | {key: .name, value: .networkInterfaces[0].accessConfigs[0].natIP}] | from_entries.\"$1\""
+		jq -r "[.[] | {key: .name, value: .networkInterfaces[0].accessConfigs[0].natIP}] | from_entries.\"$1\""
 	}
 	connect () {
 		ssh $1
@@ -31,7 +35,7 @@ gssh () {
 	fi
 
 	gcloud compute instances start $NAME
-	ADDR=$(fetch_list | get_addr $NAME)
-	nc -z $ADDR 22
-	connect $ADDR ${@:2}
+	sleep 5
+
+	connect $(fetch_list | get_addr $NAME) ${@:2}
 }
