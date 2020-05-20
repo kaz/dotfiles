21f8f9f73a596b2c788afdc1584ad39a99fd6aad
Kazuki Sawada <kazuki@6715.jp>
2020-05-20 23:04:19 +0900

    update gcreate command

--- files/functions/gcreate.fish
+++ files/functions/gcreate.fish
@@ -3,11 +3,11 @@ function gcreate -a NAME TYPE ZONE
 		set NAME "default"
 	end
 	if [ -z $TYPE ]
-		set TYPE "1"
+		set TYPE "e2-medium"
 	end
 	if [ -z $ZONE ]
-		set ZONE "b"
+		set ZONE "asia-northeast1-b"
 	end
 
-	gcloud compute instances create $NAME --machine-type=n1-standard-$TYPE --zone=asia-northeast1-$ZONE --image-project=arch-linux-gce --image-family=arch --boot-disk-size=30GB --preemptible
+	gcloud compute instances create $NAME --machine-type=$TYPE --zone=$ZONE --image-project=arch-linux-gce --image-family=arch --boot-disk-size=30GB --preemptible
 end
