41e5212aae96789de7d07e19045bb8c35f1e736c
Kazuki Sawada <kazuki@6715.jp>
2020-09-03 11:54:18 +0900

    change default instance size

--- files/functions/gcreate.fish
+++ files/functions/gcreate.fish
@@ -1,13 +1,16 @@
-function gcreate -a NAME TYPE ZONE
+function gcreate -a NAME TYPE ZONE SIZE
 	if [ -z $NAME ]
 		set NAME "default"
 	end
 	if [ -z $TYPE ]
-		set TYPE "e2-medium"
+		set TYPE "e2-micro"
 	end
 	if [ -z $ZONE ]
 		set ZONE "asia-northeast1-b"
 	end
+	if [ -z $ZONE ]
+		set SIZE "10GB"
+	end
 
-	gcloud compute instances create $NAME --machine-type=$TYPE --zone=$ZONE --image-project=arch-linux-gce --image-family=arch --boot-disk-size=30GB --preemptible
+	gcloud compute instances create $NAME --machine-type=$TYPE --zone=$ZONE --image-project=arch-linux-gce --image-family=arch --boot-disk-size=$SIZE --preemptible
 end
