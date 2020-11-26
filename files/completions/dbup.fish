1038d2896e9f3fc7023d5ec393f5936fc1d2d2ff
Kazuki Sawada <kazuki@6715.jp>
2020-11-27 01:09:48 +0900

    remove unused code

--- files/completions/dbup.fish
+++
@@ -1,2 +0,0 @@
-complete -x -c dbup -n "__fish_is_first_arg" -a "(ls ~/.database)"
-complete -x -c dbup -n "not __fish_is_first_arg" -a ""
--- files/functions/devcontainer.fish
+++
@@ -1,3 +0,0 @@
-function devcontainer
-	ln -sf ~/.devcontainer .
-end
