46ac1394dbceb8e30d7877062c34c54bd9a0c911
Kazuki Sawada <kazuki@6715.jp>
2021-06-28 15:14:07 +0900

    update prompt

--- files/functions/fish_prompt.fish
+++ files/functions/fish_prompt.fish
@@ -1,3 +1,7 @@
 function fish_prompt
-	echo -n (set_color FFFFFF)(set_color -b 005FFF) (prompt_hostname) (set_color -b 005FD7) $USER (set_color -b 005FAF) (prompt_pwd) (set_color normal)(set_color -o 005F87) \$ (set_color normal)
+	if [ $status = 0 ]
+		echo -n (set_color FFFFFF)(set_color -b 005FFF) (prompt_pwd) (set_color normal)(set_color -o 005FD7) \$ (set_color normal)
+	else
+		echo -n (set_color FFFFFF)(set_color -b FF5F00) (prompt_pwd) (set_color normal)(set_color -o D75F00) \$ (set_color normal)
+	end
 end
