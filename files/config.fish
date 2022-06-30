4d6bd264413e79850f2602292c6a06a111c02cf0
Kazuki Sawada <kazuki@6715.jp>
2022-06-30 21:45:06 +0900

    backup history file

--- files/config.fish
+++ files/config.fish
@@ -67,3 +67,8 @@ end
 
 # apply PATH to lanchctl
 sudo launchctl config user path (echo $PATH | tr " " ":") > /dev/null &
+
+# backup history after each command
+function __postexec_backup_history --on-event fish_postexec
+	backup_history &
+end
---
+++ files/functions/backup_history.fish
@@ -0,0 +1,27 @@
+function backup_history
+	history merge
+	history save
+
+	set BACKUP /tmp/fish_history.(date +%s).zstd
+	zstdmt -q -f -o "$BACKUP" "$HOME/.local/share/fish/fish_history"
+
+	__backup_history_in_dir "$BACKUP" "$HOME/Library/Mobile Documents/com~apple~CloudDocs/fish_history_backup"
+
+	set GOOGLE_DRIVE_MOUNT_PATH "$HOME/Google Drive/マイドライブ"
+	if [ -d "$GOOGLE_DRIVE_MOUNT_PATH" ]
+		__backup_history_in_dir "$BACKUP" "$GOOGLE_DRIVE_MOUNT_PATH/archive/fish_history_backup"
+	else
+		echo "backup_history: Google Drive is not mounted."
+	end
+
+	rm "$BACKUP"
+end
+
+function __backup_history_in_dir -a BACKUP DIR
+	if [ ! -d "$DIR" ]
+		mkdir -p "$DIR"
+	end
+
+	cp "$BACKUP" "$DIR"
+	ls -t "$DIR" | tail -n +1001 | xargs -I {} rm "$DIR/{}"
+end
