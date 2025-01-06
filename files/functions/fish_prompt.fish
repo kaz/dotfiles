8c3e6ecc2b659afd067f0d03eec377653d726ec3
Kazuki Sawada <kazuki@6715.jp>
2025-01-06 16:26:00 +0900

    terminal

--- files/functions/fish_prompt.fish
+++ files/functions/fish_prompt.fish
@@ -1,7 +1,7 @@
 function fish_prompt
 	if [ $status = 0 ]
-		echo -n (set_color FFFFFF)(set_color -b 005FFF) (prompt_pwd) (set_color normal)(set_color -o 005FD7) \$ (set_color normal)
+		echo -n (set_color -b 06F)(set_color FFF) (prompt_pwd) (set_color -b 00F)(set_color 06F)\uE0B0 (set_color FFF)\$ (set_color -b normal)(set_color 00F)\uE0B0 (set_color normal)
 	else
-		echo -n (set_color FFFFFF)(set_color -b FF5F00) (prompt_pwd) (set_color normal)(set_color -o D75F00) \$ (set_color normal)
+		echo -n (set_color -b F60)(set_color FFF) (prompt_pwd) (set_color -b F00)(set_color F60)\uE0B0 (set_color FFF)\$ (set_color -b normal)(set_color F00)\uE0B0 (set_color normal)
 	end
 end
