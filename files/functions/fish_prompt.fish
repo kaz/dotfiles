2447010c1263eb552c728c7c7bf8041859214c4f
Kazuki Sawada <kazuki@6715.jp>
2025-01-10 17:37:07 +0900

    propmtをpurpleにした

--- files/functions/fish_prompt.fish
+++ files/functions/fish_prompt.fish
@@ -1,7 +1,14 @@
 function fish_prompt
-	if [ $status = 0 ]
-		echo -n (set_color -b 06F)(set_color FFF) (prompt_pwd) (set_color -b 00F)(set_color 06F)\uE0B0 (set_color FFF)\$ (set_color -b normal)(set_color 00F)\uE0B0 (set_color normal)
-	else
-		echo -n (set_color -b F60)(set_color FFF) (prompt_pwd) (set_color -b F00)(set_color F60)\uE0B0 (set_color FFF)\$ (set_color -b normal)(set_color F00)\uE0B0 (set_color normal)
+	set PROMPT_PWD (prompt_pwd)
+	set PROMPT_PWD_L (string sub -l 1 $PROMPT_PWD)
+	set PROMPT_PWD_R (string replace -a "/" (printf " \uE0B1 ") (string sub -s 2 $PROMPT_PWD))
+	set PROMPT_PWD (string join "" $PROMPT_PWD_L $PROMPT_PWD_R)
+	echo -n (set_color -b 6C5793)(set_color FFF) $PROMPT_PWD (set_color -b 4B3D66)(set_color 6C5793)\uE0B0 (set_color FFF)\$ (set_color -b normal)(set_color 4B3D66)\uE0B0 (set_color normal)
+end
+
+function fish_right_prompt
+	set VCS_PROMPT (fish_vcs_prompt)
+	if [ -n "$VCS_PROMPT" ]
+		echo -n " "(set_color C2B7D5)\uE0B2(set_color -b C2B7D5)(set_color 000)$VCS_PROMPT (set_color -b normal)(set_color normal)
 	end
 end
