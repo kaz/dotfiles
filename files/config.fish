b1eb56a51810825294e437cc15aba65e37a096b9
Kazuki Sawada <kazuki@6715.jp>
2025-08-27 22:14:32 +0900

    use bun to install npm pkgs

--- files/config.fish
+++ files/config.fish
@@ -32,15 +32,14 @@ if [ -d $RUBY_PATH ]
 	fish_add_path -g $RUBY_PATH
 end
 
-# npm
-set NPM_PATH "$HOME/.npm/bin"
-if [ -d $NPM_PATH ]
-	fish_add_path -g $NPM_PATH
+# bun
+set BUN_PATH "$HOME/.bun/bin"
+if [ -d $BUN_PATH ]
+	fish_add_path -g $BUN_PATH
 end
 
-alias npm="pnpm"
-alias npx="pnpm dlx"
 alias pnpx="pnpm dlx"
+alias bunx="bun x"
 
 set -gx NPM_PKG_GITHUB_PAT "op://umtukmxolngqhardqo4ixmqnqe/2qz67nq4nbufxi73cbcwg2w7xi/token"
 set -gx NPM_PKG_P8N_AUTH "op://umtukmxolngqhardqo4ixmqnqe/6h4c3qo535ixozij4fdom55gre/token"
